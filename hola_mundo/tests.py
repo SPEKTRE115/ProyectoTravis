1d65db55acb7de55a8e6759dcdfee3277d35510f
Eduardo Aguilar Yáñez <eduardoay115@gmail.com>
2021-02-18 01:17:27 +0000

    Corrección test

--- hola_mundo/tests.py
+++ hola_mundo/tests.py
@@ -1,8 +1,11 @@
-from django.test import TestCase
+import unittest
 
 # Create your tests here.
 
-class TestSmokeTest(TestCase):
+class TestSmokeTest(unittest.TestCase):
 
     def test_uno_mas_uno_igual_a_dos(self):
         self.assertEqual(1 + 1,2)
+
+if __name__ == '__main__':
+    unittest.main()
